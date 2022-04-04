c6c60f8647e7772961b1d6424aa75e0d2252b1f6
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2022-04-04 21:40:10 +0700

    Force portrait mode to Main View Controller

--- fotoskuy/ViewController.swift
+++ fotoskuy/ViewController.swift
@@ -30,5 +30,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        AppDelegate.AppUtility.lockOrientation(UIInterfaceOrientationMask.portrait, andRotateTo: UIInterfaceOrientation.portrait)
+    }
 }
 

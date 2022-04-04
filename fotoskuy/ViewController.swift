4b94732fe3536f40332b8362766c2e327ec3b7c4
Michael Ricky <37214301+djtyranix@users.noreply.github.com>
2022-04-04 17:32:49 +0700

    Init Camera Slicing

--- fotoskuy/ViewController.swift
+++ fotoskuy/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let altTitle: String = "My name is Michael, you know."
+    let helloWorld: String = "Hello, World!"
+    
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    @IBAction func btnClickMe(_ sender: UIButton) {
+        replaceTitle(title: helloWorld)
+    }
+    
+    @IBAction func btnClickThis(_ sender: UIButton) {
+        replaceTitle(title: altTitle)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func replaceTitle(title: String) {
+        labelTitle.text = title
     }
 
 

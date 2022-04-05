88ee114b24c9b21a40c3c03f9d4c78687d96e4e5
elvinajc <elvina14jc@gmail.com>
2022-04-05 11:49:28 +0700

    Commit info sheet

---
+++ fotoskuy/InfoSheetViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InfoSheetViewController.swift
+//  fotoskuy
+//
+//  Created by Elvina Jacia on 05/04/22.
+//
+
+import UIKit
+
+class InfoSheetViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+
+}
+
+

08bf7f6412bec1a1e29758ed6b6c08f15b858e8e
Kevin Harijanto <kevinhari777@gmail.com>
2022-04-08 18:49:28 +0700

    Gallery View Placeholder & Add Navigation Controller

--- fotoskuy/ViewController/GalleryViewController.swift
+++ fotoskuy/ViewController/GalleryViewController.swift
@@ -5,12 +5,43 @@
 //  Created by Kevin Harijanto on 07/04/22.
 //
 
-import UIKit
+import SwiftUI
 
-class GalleryViewController: UIViewController {
-
+class GalleryViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+  
+    
+    private var collectionView: UICollectionView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+// bikin 3x3 grid
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
+        layout.itemSize = CGSize(width: (view.frame.size.width/3)-4, height: (view.frame.size.width/3)-4)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+    }
+    
+    //banyak placeholder
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt IndexPath: IndexPath) -> UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.identifier, for: IndexPath) as! CustomCollectionViewCell
+        cell.configure(label: "Custom \(IndexPath.row)")
+        return cell
     }
 }
